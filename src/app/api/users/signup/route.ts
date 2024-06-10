931747254523a8fda3a50f1669526bde3f12b030
Akhilesh Parmar <akhileshparmar.d4@gmail.com>
2024-06-10 11:36:18 +0530

    created route for signup

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,40 @@
+import { connectDB } from "@/dbConfig/dbConfig"
+import User from "@/models/userModel"
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs"
+import { sendEmail } from "@/helpers/mailer";
+
+
+connectDB();
+
+export async function POST(req: NextRequest) {
+  try {
+    const reqBody = await req.json();
+
+    const { username, email, password } = reqBody;
+
+    const user = await User.findOne({ email })
+
+    if (user) {
+      return NextResponse.json({ message: "user already exists" })
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      email, username, password: hashedPassword
+    })
+
+    const savedUser = newUser.save();
+
+    console.log("file: route.ts:31 ~ savedUser:", savedUser)
+
+    // await sendEmail() //send mail here
+
+    return NextResponse.json({ message: "user registered successfully" }, { status: 201 })
+
+  } catch (error: any) {
+    return NextResponse.json({ error, message: error.message }, { status: 500 })
+  }
+}
